19a5bfb15993c5a805b7900edfb1d22efb3df640
Cedric Barre <cedbarr@gmail.com>
2021-11-02 19:13:53 -0400

    Added feature method prototypes

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,5 +1,30 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import ca.mcgill.ecse.climbsafe.model.*;
+
 public class AssignmentController {
 
+	public void initiateAssignments() {
+		
+	}
+	
+	public void payTrip( Member m )
+	{
+		
+	}
+	
+	public void startTrips( int week )
+	{
+		
+	}
+	
+	public void finishTrip( Member m )
+	{
+		
+	}
+	
+	public void cancelTrip( Member m )
+	{
+		
+	}
 }
