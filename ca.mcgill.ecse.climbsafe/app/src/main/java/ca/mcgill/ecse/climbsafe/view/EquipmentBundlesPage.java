530185abc58ad37492164935b87d83da3155fd85
HabibJarweh <89218041+HabibJarweh@users.noreply.github.com>
2021-11-27 14:04:28 -0500

    updated equipment bundle page, still doesn't work

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/EquipmentBundlesPage.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/EquipmentBundlesPage.java
@@ -1,15 +1,12 @@
 package ca.mcgill.ecse.climbsafe.view;
 
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet1Controller;
 import ca.mcgill.ecse.climbsafe.controller.MiscellaneousController;
-import ca.mcgill.ecse.climbsafe.model.BookedItem;
 import ca.mcgill.ecse.climbsafe.model.BundleItem;
 import ca.mcgill.ecse.climbsafe.model.Equipment;
 import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
 import ca.mcgill.ecse.climbsafe.model.Member;
-import ca.mcgill.ecse.climbsafe.view.MembersPage.MemberPanel;
-import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet1Controller;
-import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet2Controller;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet5Controller;
 import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet6Controller;
 
@@ -27,14 +24,12 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class EquipmentBundlesPage implements Page{
-	
-//	EquipmentBundle equipmentbundle1 = new EquipmentBundle("bundle1", 25, ClimbSafeApplication.getClimbSafe());
-//	EquipmentBundle equipmentbundle2 = new EquipmentBundle("bundle2", 35, ClimbSafeApplication.getClimbSafe());
-//	EquipmentBundle equipmentbundle3 = new EquipmentBundle("bundle3", 15, ClimbSafeApplication.getClimbSafe());
 
     private GroupLayout layout;
     private JPanel panel;
+
     private JLabel label;
+
     private EquipmentBundlePanel equipmentBundlePanel;
     private EquipmentBundleSelector equipmentBundleSelector;
 
@@ -45,28 +40,29 @@ public class EquipmentBundlesPage implements Page{
     }
 
     private void initComponents(){
-        label = new JLabel("<HTML><U>Equipment Bundles</U></HTML>");
-        String[] bundleNames = new String[ClimbSafeApplication.getClimbSafe().getBundles().size()];
-        for(int i = 0; i < ClimbSafeApplication.getClimbSafe().getBundles().size(); i++){
-            bundleNames[i] = ClimbSafeApplication.getClimbSafe().getBundle(i).getName();
+        label = new JLabel("<HTML><B><U>Equipment Bundles</U></B></HTML>");
+        String[] equipmentBundleNames = new String[ClimbSafeApplication.getClimbSafe().getBundles().size()];
+        for(int i = 0; i < ClimbSafeApplication.getClimbSafe().getMembers().size(); i++){
+            equipmentBundleNames[i] = ClimbSafeApplication.getClimbSafe().getBundle(i).getName();
         }
         equipmentBundlePanel = new EquipmentBundlePanel();
-        equipmentBundleSelector = new EquipmentBundleSelector(bundleNames,
+        equipmentBundleSelector = new EquipmentBundleSelector(equipmentBundleNames,
                 (selected) -> {
                     panel.remove(equipmentBundlePanel);
                     equipmentBundlePanel = new EquipmentBundlePanel(ClimbSafeApplication.getClimbSafe().findEquipmentBundleFromName(selected));
                     makeLayout();
                 },
-                (name) -> {
+                (email) -> {
                     panel.remove(equipmentBundlePanel);
-                    equipmentBundlePanel = new EquipmentBundlePanel(name);
+                    equipmentBundlePanel = new EquipmentBundlePanel(email);
+
                     makeLayout();
                 }
         );
         makeLayout();
     }
-    
-    private void removeMemberPanel(){
+
+    private void removeEquipmentBundlePanel(){
         panel.remove(equipmentBundlePanel);
         equipmentBundlePanel = new EquipmentBundlePanel();
         makeLayout();
@@ -103,19 +99,20 @@ public class EquipmentBundlesPage implements Page{
 
     class EquipmentBundleSelector extends JPanel{
 
-        String[] bundleNames;
+        String[]equipmentBundleNames;
         Consumer<String> select;
+
         GroupLayout barLayout;
         JList bar;
+        JTextField addEmailField;
         JButton addButton;
         JButton deleteButton;
-        JTextField addnameField;
 
-        public EquipmentBundleSelector(String[] bundleNames, Consumer<String> select, Consumer<String> addEvent){
-            this.bundleNames = bundleNames;
+        public EquipmentBundleSelector(String[] equipmentBundleNames, Consumer<String> select, Consumer<String> addEvent){
+            this.equipmentBundleNames = equipmentBundleNames;
             this.select = select;
 
-            bar = new JList(bundleNames);
+            bar = new JList(equipmentBundleNames);
             bar.addListSelectionListener(new ListSelectionListener() {
                 @Override
                 public void valueChanged(ListSelectionEvent e) {
@@ -125,10 +122,8 @@ public class EquipmentBundlesPage implements Page{
             bar.setFont(bar.getFont().deriveFont(Font.PLAIN, 15.0f));
             bar.setFixedCellHeight(30);
             bar.setBorder(BorderFactory.createEmptyBorder());
-            
-            addButton = new JButton("Add");
-            
-            addnameField = new JTextField();
+
+            addEmailField = new JTextField();
             addButton = new JButton("Add");
             deleteButton = new JButton("Delete");
             deleteButton.addActionListener(new ActionListener() {
@@ -137,23 +132,22 @@ public class EquipmentBundlesPage implements Page{
                     ClimbSafeFeatureSet6Controller.deleteEquipmentBundle(String.valueOf(bar.getSelectedValue()));
                     removeSelectedItem();
                     makeNewBar();
-                    removeMemberPanel();
+                    removeEquipmentBundlePanel();
                 }
             });
 
             addButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    addEvent.accept(addnameField.getText());
+                    addEvent.accept(addEmailField.getText());
                 }
             });
-            
-                makeLayout();
-            }
-        
+            makeLayout();
+        }
+
         private void makeNewBar(){
             remove(bar);
-            bar = new JList(bundleNames);
+            bar = new JList(equipmentBundleNames);
             bar.setFont(bar.getFont().deriveFont(Font.PLAIN, 15.0f));
             bar.setFixedCellHeight(30);
             bar.setBorder(BorderFactory.createEmptyBorder());
@@ -165,105 +159,119 @@ public class EquipmentBundlesPage implements Page{
             });
             makeLayout();
         }
-        
+
         private void makeLayout(){
             barLayout = new GroupLayout(this);
             barLayout.setHorizontalGroup(
                     barLayout.createParallelGroup()
                             .addComponent(bar)
-                            .addComponent(addnameField)
+                            .addComponent(addEmailField)
                             .addComponent(addButton)
                             .addComponent(deleteButton)
             );
             barLayout.setVerticalGroup(
                     barLayout.createSequentialGroup()
                             .addComponent(bar)
-                            .addComponent(addnameField)
+                            .addComponent(addEmailField)
                             .addComponent(addButton)
                             .addComponent(deleteButton)
             );
             setLayout(barLayout);
             updateUI();
         }
-        
-        
+
         private void removeSelectedItem(){
-            String[] newBundles = new String[bundleNames.length-1];
+            String[] newEquipmentBundles = new String[equipmentBundleNames.length-1];
             int j = 0;
-            for(String b: bundleNames){
-                if(b == bar.getSelectedValue()) continue;
-                newBundles[j] = b;
+            for(String m: equipmentBundleNames){
+                if(m == bar.getSelectedValue()) continue;
+                newEquipmentBundles[j] = m;
                 j++;
             }
-            bundleNames = newBundles;
+            equipmentBundleNames = newEquipmentBundles;
         }
+
     }
 
 
 
     class EquipmentBundlePanel extends JPanel{
 
-        private EquipmentBundle equipmentBundle;
         private boolean newEquipmentBundle;
-        private String newName= "";
-        private String oldname = "";
+        private String newName = "";
+        private String oldName = "";
+        private EquipmentBundle equipmentBundle;
 
         private JLabel name;
         private JLabel discount;
-        private JLabel oldName;
 
         private JTextField enterName;
         private JTextField enterDiscount;
-
 
         private EquipmentSelector equipmentSelector;
 
         private JButton saveButton;
 
-        private GroupLayout bundleInfoLayout;
-        
-        public EquipmentBundlePanel() {}
+        private GroupLayout equipmentBundleInfoLayout;
+
+        public EquipmentBundlePanel(){}
 
         public EquipmentBundlePanel(String name){
-        	newEquipmentBundle = true;
-        	newName = name;
-        	
-        	makeLayout();
-        	
+            newEquipmentBundle = true;
+            oldName = newName;
+            newName = name;
+
+            makeLayout();
         }
 
         public EquipmentBundlePanel(EquipmentBundle equipmentBundle){
             this.equipmentBundle = equipmentBundle;
-            oldname = equipmentBundle.getName();
-            
+
             newEquipmentBundle = false;
+
             makeLayout();
         }
 
-            
-            private void makeLayout() {
+        private void makeLayout(){
+//            email = new JLabel ("Email:");
+//            password = new JLabel ("Password:");
             name = new JLabel ("Name:");
-            discount = new JLabel ("Discount");
+            discount = new JLabel ("Discount:  ");
+//            nrOfWeeks = new JLabel ("Number of weeks:");
+//            guideRequired = new JLabel ("Guide Required:  ");
+//            hotelRequired = new JLabel ("Hotel Required:  ");
 
-            enterName = new JTextField (equipmentBundle.getName());
-            enterDiscount = new JTextField (equipmentBundle.getDiscount());
-            
             if(newEquipmentBundle){
+//                enterEmail = new JLabel(newEmail);
+//                enterEmail.setFont(enterEmail.getFont().deriveFont(Font.PLAIN));
+//                enterPassword = new JTextField();
                 enterName = new JTextField();
                 enterDiscount = new JTextField();
+//                enterNrOfWeeks = new JTextField();
+//                enterGuideRequired = new JToggleButton("Required");
+//                enterGuideRequired.setSelected(false);
+//                enterHotelRequired = new JToggleButton("Required");
+//                enterHotelRequired.setSelected(false);
             } else {
-            	oldName = new JLabel(equipmentBundle.getName());
-            	oldName.setFont(oldName.getFont().deriveFont(Font.PLAIN)); 
-            	enterName = new JTextField(equipmentBundle.getName());
+//                enterEmail = new JLabel(member.getEmail());
+//                enterEmail.setFont(enterEmail.getFont().deriveFont(Font.PLAIN));
+//                enterPassword = new JTextField(member.getPassword());
+                enterName = new JTextField(equipmentBundle.getName());
                 enterDiscount = new JTextField(equipmentBundle.getDiscount());
+//                enterNrOfWeeks = new JTextField(String.valueOf(member.getNrWeeks()));
+//                enterGuideRequired = new JToggleButton("Required");
+//                enterGuideRequired.setSelected(member.getGuideRequired());
+//                enterHotelRequired = new JToggleButton("Required");
+//                enterHotelRequired.setSelected(member.getHotelRequired());
             }
 
             ArrayList<String> equipmentNames = new ArrayList<>();
             ArrayList<Integer> equipmentQuantities = new ArrayList<>();
-            for(BundleItem b: equipmentBundle.getBundleItems()){
-                equipmentNames.add(b.getEquipment().getName());
-                equipmentQuantities.add(b.getQuantity());
-            }
+            if(!newEquipmentBundle)
+                for(BundleItem b: equipmentBundle.getBundleItems()){
+                    equipmentNames.add(b.getEquipment().getName());
+                    equipmentQuantities.add(b.getQuantity());
+                }
             equipmentSelector = new EquipmentSelector(equipmentNames, equipmentQuantities);
             equipmentSelector.setBorder(new EmptyBorder(0, 100, 0, 0));
 
@@ -275,43 +283,43 @@ public class EquipmentBundlesPage implements Page{
                 }
             });
 
-            bundleInfoLayout = new GroupLayout(this);
-            bundleInfoLayout.setHorizontalGroup(
-                    bundleInfoLayout.createSequentialGroup()
+            equipmentBundleInfoLayout = new GroupLayout(this);
+            equipmentBundleInfoLayout.setHorizontalGroup(
+            		equipmentBundleInfoLayout.createSequentialGroup()
                             .addGroup(
-                                bundleInfoLayout.createParallelGroup()
-                                        .addComponent(name)
-                                        .addComponent(discount)
-                                        .addComponent(saveButton)
+                            		equipmentBundleInfoLayout.createParallelGroup()
+                                            .addComponent(name)
+                                            .addComponent(discount)
+                                            .addComponent(saveButton)
                             )
                             .addGroup(
-                                    bundleInfoLayout.createParallelGroup()
+                            		equipmentBundleInfoLayout.createParallelGroup()
                                             .addComponent(enterName)
                                             .addComponent(enterDiscount)
                             )
                             .addComponent(equipmentSelector)
             );
-            bundleInfoLayout.setVerticalGroup(
-                    bundleInfoLayout.createParallelGroup()
-                                    .addGroup(
-                                        bundleInfoLayout.createSequentialGroup()
-                                                .addGroup(
-                                                        bundleInfoLayout.createParallelGroup()
-                                                                .addComponent(name)
-                                                                .addComponent(enterName)
-                                                )
-                                                .addGroup(
-                                                        bundleInfoLayout.createParallelGroup()
-                                                                .addComponent(discount)
-                                                                .addComponent(enterDiscount)
-                                                )
-                                                .addComponent(saveButton)
-                                    )
+            equipmentBundleInfoLayout.setVerticalGroup(
+            		equipmentBundleInfoLayout.createParallelGroup()
+                            .addGroup(
+                            		equipmentBundleInfoLayout.createSequentialGroup()
+                                            .addGroup(
+                                            		equipmentBundleInfoLayout.createParallelGroup()
+                                                            .addComponent(name)
+                                                            .addComponent(enterName)
+                                            )
+                                            .addGroup(
+                                            		equipmentBundleInfoLayout.createParallelGroup()
+                                                            .addComponent(discount)
+                                                            .addComponent(enterDiscount)
+                                            )
+                                            .addComponent(saveButton)
+                            )
                             .addComponent(equipmentSelector)
             );
-            setLayout(bundleInfoLayout);
-
+            setLayout(equipmentBundleInfoLayout);
         }
+
         private void SaveModification(){
             equipmentSelector.getEquipmentQuantities().keySet(); //gets the items that have quantities
             equipmentSelector.getEquipmentQuantities().values(); //gets the quantities of the items
@@ -320,21 +328,31 @@ public class EquipmentBundlesPage implements Page{
             ArrayList<String> items = new ArrayList<String>();
             items.addAll(equipmentSelector.getEquipmentQuantities().keySet());
             try {
-                
                 if (newEquipmentBundle) {
                     ClimbSafeFeatureSet5Controller.addEquipmentBundle(
+//                            enterEmail.getText(),
+//                            enterPassword.getText(),
                             enterName.getText(),
                             Integer.parseInt(enterDiscount.getText()),
+//                            Integer.parseInt(enterNrOfWeeks.getText()),
+//                            enterGuideRequired.isSelected(),
+//                            enterHotelRequired.isSelected(),
                             items,
                             quantities
                     );
-                } else { ClimbSafeFeatureSet5Controller.updateEquipmentBundle(
-                        oldname,
-                		enterName.getText(),
-                        Integer.parseInt(enterDiscount.getText()),
-                        items,
-                        quantities
-                );
+                } else {
+                    ClimbSafeFeatureSet5Controller.updateEquipmentBundle(
+//                            enterEmail.getText(),
+//                            enterPassword.getText(),
+                    		oldName,
+                            enterName.getText(),
+                            Integer.parseInt(enterDiscount.getText()),
+//                            Integer.parseInt(enterNrOfWeeks.getText()),
+//                            enterGuideRequired.isSelected(),
+//                            enterHotelRequired.isSelected(),
+                            items,
+                            quantities
+                    );
                 }
             } catch(Exception e){
                 e.printStackTrace();
