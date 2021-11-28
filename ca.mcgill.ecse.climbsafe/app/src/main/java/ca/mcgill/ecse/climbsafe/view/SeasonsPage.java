b75498c7c3f17a97c78c470cd87c480fa1c86e34
Zachary Godden <74373869+ZGOD2001@users.noreply.github.com>
2021-11-28 16:26:58 -0500

    Implemented text and non working button for Seasons view

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/SeasonsPage.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/SeasonsPage.java
@@ -1,17 +1,54 @@
 package ca.mcgill.ecse.climbsafe.view;
 
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
 
-public class SeasonsPage implements Page{
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+
+public class SeasonsPage implements Page {
 
     private GroupLayout layout;
     private JPanel panel;
+    private JLabel startDateLabel;
+    private JLabel numWeeksLabel;
+    private JLabel priceOfGuideLabel;
+    private JLabel startDate;
+    private JLabel weeks;
+    private JLabel priceOfGuide;
+    private JButton editStart = new JButton("edit");
+    private JButton editWeeks = new JButton("edit");
+    private JButton editPriceGuide = new JButton("edit");
+
+    Properties j = new Properties();
+    OutputStream os;
+    JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+
 
     public SeasonsPage(){
+        Date start = ClimbSafeApplication.getClimbSafe().getStartDate();
+        Integer numWeeks = ClimbSafeApplication.getClimbSafe().getNrWeeks();
+        Integer priceGuide =ClimbSafeApplication.getClimbSafe().getPriceOfGuidePerWeek();
         panel = new JPanel();
         layout = new GroupLayout(panel);
+        startDateLabel = new JLabel("Start Date:");
+        startDate = new JLabel(start.toString());
+        weeks = new JLabel(numWeeks.toString());
+        priceOfGuide = new JLabel(priceGuide.toString());
+
+        numWeeksLabel = new JLabel("Number of Weeks:");
+        priceOfGuideLabel = new JLabel("Price of Guide Per Week:");
         initComponents();
+        editStart.addActionListener( e -> startButtonPressed() );
     }
 
     private void initComponents(){
@@ -21,12 +58,44 @@ public class SeasonsPage implements Page{
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
         layout.setHorizontalGroup(
-                layout.createSequentialGroup()
+                layout.createParallelGroup()
                         .addComponent(label)
+                        .addGroup( layout.createSequentialGroup()
+                                .addComponent(startDateLabel)
+                                .addComponent(startDate)
+                                .addComponent(editStart)
+                        )
+
+                                .addGroup( layout.createSequentialGroup()
+                                        .addComponent(numWeeksLabel)
+                                        .addComponent(weeks)
+                                        .addComponent(editWeeks)
+                                )
+                        .addGroup( layout.createSequentialGroup()
+                                .addComponent(priceOfGuideLabel)
+                                .addComponent(priceOfGuide)
+                                .addComponent(editPriceGuide)
+                        )
         );
         layout.setVerticalGroup(
                 layout.createSequentialGroup()
                         .addComponent(label)
+                        .addGroup( layout.createParallelGroup()
+                                .addComponent(startDateLabel)
+                                .addComponent(startDate)
+                                .addComponent(editStart)
+                        )
+
+                        .addGroup( layout.createParallelGroup()
+                                .addComponent(numWeeksLabel)
+                                .addComponent(weeks)
+                                .addComponent(editWeeks)
+                        )
+                        .addGroup( layout.createParallelGroup()
+                                .addComponent(priceOfGuideLabel)
+                                .addComponent(priceOfGuide)
+                                .addComponent(editPriceGuide)
+                        )
         );
     }
 
@@ -36,4 +105,50 @@ public class SeasonsPage implements Page{
     }
 
 
+    private void startButtonPressed() {
+
+
+
+//        layout.setHorizontalGroup(
+//                layout.createParallelGroup()
+//                        .addComponent(label)
+//                        .addGroup( layout.createSequentialGroup()
+//                                .addComponent(startDateLabel)
+//                                .addComponent(startDate)
+//                                .addComponent(editStart)
+//                        )
+//
+//                        .addGroup( layout.createSequentialGroup()
+//                                .addComponent(numWeeksLabel)
+//                                .addComponent(weeks)
+//                                .addComponent(editWeeks)
+//                        )
+//                        .addGroup( layout.createSequentialGroup()
+//                                .addComponent(priceOfGuideLabel)
+//                                .addComponent(priceOfGuide)
+//                                .addComponent(editPriceGuide)
+//                        )
+//        );
+//        layout.setVerticalGroup(
+//                layout.createSequentialGroup()
+//                        .addComponent(label)
+//                        .addGroup( layout.createParallelGroup()
+//                                .addComponent(startDateLabel)
+//                                .addComponent(startDate)
+//                                .addComponent(editStart)
+//                        )
+//
+//                        .addGroup( layout.createParallelGroup()
+//                                .addComponent(numWeeksLabel)
+//                                .addComponent(weeks)
+//                                .addComponent(editWeeks)
+//                        )
+//                        .addGroup( layout.createParallelGroup()
+//                                .addComponent(priceOfGuideLabel)
+//                                .addComponent(priceOfGuide)
+//                                .addComponent(editPriceGuide)
+//                        )
+//        );
+    }
 }
+
